ed2540cdded2cc0b5ffab93c65b56085cf56f3c5
Cassie <cassie@mobify.com>
2018-06-04 15:46:34 -0700

    Clean up example

--- worker.js
+++ worker.js
@@ -7,35 +7,7 @@ const worker = () => {
     toolbox.options.cache.name = baseCacheName
     toolbox.options.cache.maxAgeSeconds = 86400
     toolbox.options.debug = true
-
-    // const cacheNames = [baseCacheName]
-
-    // Lifecycle Handlers
-    // self.addEventListener('install', (e) => {
-    //     e.waitUntil(
-    //         self.skipWaiting()
-    //             .then(() => console.log('[ServiceWorker] Installed version', version))
-    //     )
-    // })
-
-    // self.addEventListener('activate', (e) => {
-    //     e.waitUntil(
-    //         self.clients.claim()
-    //             .then(() => caches.keys())
-    //             .then((cacheKeys) => {
-    //                 return cacheKeys.filter(
-    //                     (key) => cacheNames.indexOf(key) === -1 &&
-    //                         !key.endsWith('$$$inactive$$$')
-    //                 )
-    //             })
-    //             .then((keysToDelete) => keysToDelete.map((key) => caches.delete(key)))
-    //             .then((promises) => Promise.all(promises))
-    //     )
-    // })
-
-    const useCache = /cache=true/.test(self.location.search)
-
-    toolbox.router.default = toolbox.networkOnly // useCache ? toolbox.networkFirst : toolbox.networkOnly
+    toolbox.router.default = toolbox.networkOnly
 }
 
 worker()
